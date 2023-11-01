f2274d6279aea2fade227ee2597014d5cc6617a3
Nikita0703 <nikita.noviczenko2005@gmail.com>
2023-11-01 19:11:04 +0300

    make facade

---
+++ src/main/java/com/example/nikita/facade/EmployeeFacade.java
@@ -0,0 +1,107 @@
+package com.example.nikita.facade;
+
+import com.example.nikita.dto.*;
+import com.example.nikita.entity.*;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EmployeeFacade {
+    public Employee Employee(EmployeeDTO employeedto){
+        Car car = Car.builder().
+                model(employeedto.getCar().getModel())
+                .made(employeedto.getCar().getMade()).
+                build();
+
+        House house = House.builder()
+                .adress(employeedto.getHouse().getAdress())
+                .flour(employeedto.getHouse().getFlour())
+                .flat(employeedto.getHouse().getFlat())
+                .build();
+
+        List<Pet> pets = new ArrayList<>();
+
+        for (PetDTO petDTO:employeedto.getPets()){
+            Pet pet = Pet.builder().
+                    vid(petDTO.getVid())
+                    .petname(petDTO.getName()).
+                    build();
+            pets.add(pet);
+        }
+
+        List<Project> projects = new ArrayList<>();
+
+        for (ProjectDTO projectDTO:employeedto.getProjects()){
+            Project project = Project.builder().
+                    title(projectDTO.getTitle())
+                    .year(projectDTO.getYear()).
+                    build();
+            projects.add(project);
+        }
+
+        Employee employee = Employee.builder().
+                id(employeedto.getId()).
+                name(employeedto.getName())
+                .surname(employeedto.getSurname())
+                .salary(employeedto.getSalary())
+                .department(employeedto.getDepartment())
+                .car(car)
+                .house(house)
+                .pets(pets)
+                .projects(projects)
+                .build();
+
+        return employee;
+
+    }
+
+    public EmployeeDTO EmployeeDTO(Employee employee){
+        CarDTO car = CarDTO.builder().
+                model(employee.getCar().getModel())
+                .made(employee.getCar().getMade()).
+                build();
+
+        HouseDTO house = HouseDTO.builder()
+                .adress(employee.getHouse().getAdress())
+                .flour(employee.getHouse().getFlour())
+                .flat(employee.getHouse().getFlat())
+                .build();
+
+        List<PetDTO> pets = new ArrayList<>();
+
+        for (Pet peto:employee.getPets()){
+            PetDTO pet = PetDTO.builder().
+                    vid(peto.getVid())
+                    .petname(peto.getName()).
+                    build();
+            pets.add(pet);
+        }
+
+        List<ProjectDTO> projects = new ArrayList<>();
+
+        for (Project project:employee.getProjects()){
+            ProjectDTO projectDTO = ProjectDTO.builder().
+                    title(project.getTitle())
+                    .year(project.getYear()).
+                    build();
+            projects.add(projectDTO);
+        }
+
+        EmployeeDTO employeeDTO = EmployeeDTO.builder().
+                id(employee.getId()).
+                name(employee.getName())
+                .surname(employee.getSurname())
+                .salary(employee.getSalary())
+                .department(employee.getDepartment())
+                .car(car)
+                .house(house)
+                .pets(pets)
+                .projects(projects)
+                .build();
+
+        return employeeDTO;
+    }
+
+}
--- src/main/java/com/example/nikita/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/nikita/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.nikita.service;
 import com.example.nikita.dto.*;
 import com.example.nikita.entity.*;
 import com.example.nikita.exception_handling.NoSuchEmployeeException;
+import com.example.nikita.facade.EmployeeFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.nikita.dao.EmployeeDAO;
@@ -17,55 +18,17 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeDAO employeeDAO;
 
+    @Autowired
+    private EmployeeFacade employeeFacade;
+
     @Override
 
     public List<EmployeeDTO> getAllEmployees() {
         List<Employee> employees = employeeDAO.findAll();
         List<EmployeeDTO> employeeDTOS = new ArrayList<>();
         for (Employee employee:employees){
-            CarDTO car = CarDTO.builder().
-                    model(employee.getCar().getModel())
-                    .made(employee.getCar().getMade()).
-                    build();
 
-            HouseDTO house = HouseDTO.builder()
-                    .adress(employee.getHouse().getAdress())
-                    .flour(employee.getHouse().getFlour())
-                    .flat(employee.getHouse().getFlat())
-                    .build();
-
-            List<PetDTO> pets = new ArrayList<>();
-
-            for (Pet peto:employee.getPets()){
-                PetDTO pet = PetDTO.builder().
-                        vid(peto.getVid())
-                        .petname(peto.getName()).
-                        build();
-                pets.add(pet);
-            }
-
-            List<ProjectDTO> projects = new ArrayList<>();
-
-            for (Project project:employee.getProjects()){
-                ProjectDTO projectDTO = ProjectDTO.builder().
-                        title(project.getTitle())
-                        .year(project.getYear()).
-                        build();
-                projects.add(projectDTO);
-            }
-
-            EmployeeDTO employeeDTO = EmployeeDTO.builder().
-                    id(employee.getId()).
-                    name(employee.getName())
-                    .surname(employee.getSurname())
-                    .salary(employee.getSalary())
-                    .department(employee.getDepartment())
-                    .car(car)
-                    .house(house)
-                    .pets(pets)
-                    .projects(projects)
-                    .build();
-
+            EmployeeDTO employeeDTO = employeeFacade.EmployeeDTO(employee);
             employeeDTOS.add(employeeDTO);
 
         }
@@ -74,49 +37,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee saveEmployee(EmployeeDTO employeedto) {
-        Car car = Car.builder().
-                model(employeedto.getCar().getModel())
-                        .made(employeedto.getCar().getMade()).
-                build();
-
-        House house = House.builder()
-                .adress(employeedto.getHouse().getAdress())
-                .flour(employeedto.getHouse().getFlour())
-                .flat(employeedto.getHouse().getFlat())
-                .build();
-
-        List<Pet> pets = new ArrayList<>();
-
-        for (PetDTO petDTO:employeedto.getPets()){
-            Pet pet = Pet.builder().
-                vid(petDTO.getVid())
-                    .petname(petDTO.getName()).
-                      build();
-            pets.add(pet);
-        }
-
-        List<Project> projects = new ArrayList<>();
-
-        for (ProjectDTO projectDTO:employeedto.getProjects()){
-            Project project = Project.builder().
-                    title(projectDTO.getTitle())
-                    .year(projectDTO.getYear()).
-                    build();
-            projects.add(project);
-        }
-
-        Employee employee = Employee.builder().
-                 id(employeedto.getId()).
-                 name(employeedto.getName())
-                .surname(employeedto.getSurname())
-                .salary(employeedto.getSalary())
-                .department(employeedto.getDepartment())
-                .car(car)
-                .house(house)
-                .pets(pets)
-                .projects(projects)
-                .build();
-
+        Employee employee = employeeFacade.Employee(employeedto);
         return employeeDAO.save(employee);
     }
 
@@ -131,48 +52,7 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
         }
 
-        CarDTO car = CarDTO.builder().
-                model(employee.getCar().getModel())
-                .made(employee.getCar().getMade()).
-                build();
-
-        HouseDTO house = HouseDTO.builder()
-                .adress(employee.getHouse().getAdress())
-                .flour(employee.getHouse().getFlour())
-                .flat(employee.getHouse().getFlat())
-                .build();
-
-        List<PetDTO> pets = new ArrayList<>();
-
-        for (Pet peto:employee.getPets()){
-            PetDTO pet = PetDTO.builder().
-                    vid(peto.getVid())
-                    .petname(peto.getName()).
-                    build();
-            pets.add(pet);
-        }
-
-        List<ProjectDTO> projects = new ArrayList<>();
-
-        for (Project project:employee.getProjects()){
-            ProjectDTO projectDTO = ProjectDTO.builder().
-                    title(project.getTitle())
-                    .year(project.getYear()).
-                    build();
-            projects.add(projectDTO);
-        }
-
-        EmployeeDTO employeeDTO = EmployeeDTO.builder().
-                id(employee.getId()).
-                name(employee.getName())
-                .surname(employee.getSurname())
-                .salary(employee.getSalary())
-                .department(employee.getDepartment())
-                .car(car)
-                .house(house)
-                .pets(pets)
-                .projects(projects)
-                .build();
+        EmployeeDTO employeeDTO = employeeFacade.EmployeeDTO(employee);
 
         if (employeeDTO == null) {
             throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
